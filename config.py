0fbea76ade38192d95b43bf31396e7a8a55a2535
Tom Trval <trval@kajot.cz>
2016-08-13 23:07:46 +0200

    added: fork me on github
    fix: mongo connection control before first login

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ PROFILE_LOG_LEVEL = logging.INFO
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 MONGODB_DB = 'dnd'
+MONGODB_ALIAS = 'default'
 if "MONGODB_URL" in os.environ:
     MONGODB_HOST, MONGODB_PORT = os.environ.get('MONGODB_URL', 'localhost:27017').lstrip('mongodb://').rstrip('/').split(':')
     MONGODB_PORT = int(MONGODB_PORT)
--- dnd/__init__.py
+++ dnd/__init__.py
@@ -7,7 +7,7 @@ import os
 import atexit
 from flask import Flask, send_from_directory, url_for, render_template
 from flask_restful import Api
-from flask_mongoengine import MongoEngine
+from flask_mongoengine import MongoEngine, mongoengine
 from flask_pagedown import PageDown
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_gravatar import Gravatar
@@ -45,18 +45,24 @@ class DnD(Flask):
         def not_found(error):
             return render_template('404.html', error=error), 404
         
+        self.before_first_request(self.ensure_connection)
+        
+    def ensure_connection(self):
+        mongoengine.connect(app.config['MONGODB_DB'], alias=mongoengine.connection.DEFAULT_CONNECTION_NAME, host=app.config['MONGODB_HOST'])
+        
 
 app = DnD(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 db = MongoEngine(app)
-
 pagedown = PageDown(app)
 
 toolbar = None
 if app.config.get('DEBUG_TOOLBAR_ACTIVATE', False):
     app.debug = True
+    app.config['DEBUG_TB_PANELS'] = ['flask_mongoengine.panels.MongoDebugPanel']
     toolbar = DebugToolbarExtension(app)
+    
 
 gravatar = Gravatar(app,
                     size=156,
